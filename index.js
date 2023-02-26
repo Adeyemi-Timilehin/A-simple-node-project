a8c60e2e493d088b51411c4e4e4647736e3cf320
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2023-02-26 22:37:46 +0100

    All files are working now

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+const console = require('console');
 
 http.createServer(function (req, res) {
+  
   var q = url.parse(req.url, true);
-  q=q.pathname
-//   console.log(q)
-  var filename = "." + q;
 
-  fs.readFile(filename+'.html', function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
+   q=q.pathname
+
+  var filename = "." + q;
+if(q==='/'){
+    fs.readFile('index.html',(err,data)=>{
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      });
+    }
+else {
+    fs.readFile(filename+'.html', function(err, data) {
+        if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          
+          return res.end("<h1>404 Not Found</h1>");
+        } 
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+      })
+}
+  ;
 }).listen(8080);
