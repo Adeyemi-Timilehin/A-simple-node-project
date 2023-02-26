d40f28e958ab93deaf95b5a09f9a49afddda83d6
Adeyemi Timilehin <adeyemitimmy@gmail.com>
2023-02-26 22:12:10 +0100

    index file modified

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ var fs = require('fs');
 
 http.createServer(function (req, res) {
   var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
+  q=q.pathname
+//   console.log(q)
+  var filename = "." + q;
+
+  fs.readFile(filename+'.html', function(err, data) {
     if (err) {
       res.writeHead(404, {'Content-Type': 'text/html'});
- 
       return res.end("404 Not Found");
     } 
     res.writeHead(200, {'Content-Type': 'text/html'});
